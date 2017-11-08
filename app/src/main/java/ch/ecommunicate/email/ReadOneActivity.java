6edf457917a4bf0aaad655af91f511b49e0f0693
Your Name <you@example.com>
2017-11-08 18:20:25 +0100

    add first attachment

--- app/src/main/java/ch/ecommunicate/email/ReadOneActivity.java
+++ app/src/main/java/ch/ecommunicate/email/ReadOneActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
@@ -27,6 +28,7 @@ import org.json.JSONObject;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -42,15 +44,14 @@ public class ReadOneActivity extends AppCompatActivity implements View.OnClickLi
 
     private static final String TAG = "ReadOneActivity";
 
-
-
     String email_body;
     String email_subject;
     String email_from;
     String email_cc;
     String email_date;
     String email_to;
-    String email_attachments;
+    String email_attachment1;
+    String email_attachment1_id;
 
     private Boolean sent;
     private String email_id;
@@ -79,6 +80,146 @@ public class ReadOneActivity extends AppCompatActivity implements View.OnClickLi
         }
     };
 
+    public class ReadOneActivityAsyncTask2 extends AsyncTask<String, Void, Integer> {
+
+        private static final String TAG = "ReadOneActivity";
+
+        @Override
+        protected void onPostExecute(Integer result) {
+
+
+
+        }
+
+        @Override
+        protected Integer doInBackground(String... strings) {
+            InputStream inputStream = null;
+            HttpsURLConnection urlConnection = null;
+            Integer result = 0;
+
+            String attachment_id = strings[0];
+            String filename = strings[1];
+
+            try {
+
+                URL url = new URL("https://email.android.ecommunicate.ch:443/downloadattachment/");
+                urlConnection = (HttpsURLConnection) url.openConnection();
+
+                urlConnection.setRequestProperty("Content-Type","application/json");
+
+                urlConnection.setRequestProperty("Accept","application/json");
+
+                urlConnection.setRequestMethod("POST");
+
+                urlConnection.setDoInput(true);
+
+                urlConnection.setDoOutput(true);
+
+                OutputStream os1 = urlConnection.getOutputStream();
+
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os1, "UTF-8"));
+
+                JSONObject json_object = new JSONObject();
+
+                json_object.put("attachment_id",attachment_id);
+                json_object.put("email_id",email_id);
+                json_object.put("id_token",id_token);
+                json_object.put("sent",sent);
+
+                writer.write(json_object.toString());
+
+                writer.flush();
+
+                writer.close();
+
+                urlConnection.connect();
+
+                int statusCode = urlConnection.getResponseCode();
+
+                if (statusCode == 200) {
+                    InputStream is = new BufferedInputStream(urlConnection.getInputStream(), 16384);
+
+                    Log.d(TAG,"andrew debug 1");
+
+                    OutputStream os2 = new FileOutputStream(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString() + "/" + filename);
+
+                    Log.d(TAG,"andrew debug 2");
+
+                    byte data[] = new byte[1024];
+
+                    Log.d(TAG,"andrew debug 3");
+
+                    while (true) {
+
+                        Log.d(TAG,"andrew debug 4");
+
+                        int return_value = is.read(data);
+
+                        Log.d(TAG,"andrew debug 5");
+
+                        if (return_value == -1)
+                            break;
+
+                        Log.d(TAG,"andrew debug 6");
+
+                        os2.write(data, 0, return_value);
+
+                        Log.d(TAG,"andrew debug 6");
+                    }
+
+                    os2.flush();
+
+                    os2.close();
+
+                    is.close();
+
+                    result = 1;
+
+                } else {
+
+                    result = 0;
+
+                }
+            }
+            catch (Exception e) {
+
+                if (e.getMessage() != null) {
+                    Log.d(TAG, e.getMessage());
+                }
+
+                if (e.getLocalizedMessage() != null) {
+                    Log.d(TAG, e.getLocalizedMessage());
+                }
+
+                if (e.getCause() != null) {
+                    Log.d(TAG, e.getCause().toString());
+                }
+
+                e.printStackTrace();
+
+                result = 0;
+            }
+
+            return result;
+        }
+
+        private String convertInputStreamToString(InputStream inputStream) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+            String line = "";
+            String result = "";
+            while((line = bufferedReader.readLine()) != null){
+                result += line;
+            }
+
+            if(null!=inputStream){
+                inputStream.close();
+            }
+            return result;
+        }
+
+    }
+
     public class ReadOneActivityAsyncTask1 extends AsyncTask<String, Void, Integer> {
 
         private static final String TAG = "ROActivityAsyncTask1";
@@ -98,10 +239,30 @@ public class ReadOneActivity extends AppCompatActivity implements View.OnClickLi
             date.setText(email_date);
             TextView subject = (TextView)findViewById(R.id.subject);
             subject.setText(email_subject);
-            TextView attachments = (TextView)findViewById(R.id.attachments);
-            attachments.setText(email_attachments);
+            Button attachment1 = (Button)findViewById(R.id.attachment1);
 
+            if (email_attachment1 != "") {
 
+                attachment1.setText(email_attachment1);
+
+                attachment1.setTag(email_attachment1_id);
+
+                attachment1.setVisibility(View.VISIBLE);
+
+                attachment1.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+
+                        Button button = (Button) v;
+
+                        new ReadOneActivityAsyncTask2().execute((String) button.getTag(), (String) button.getText());
+
+                    }
+                });
+
+            } else {
+
+                attachment1.setVisibility(View.INVISIBLE);
+            }
 
         }
 
@@ -232,7 +393,14 @@ public class ReadOneActivity extends AppCompatActivity implements View.OnClickLi
 
                 if (email_json.getJSONArray("attachments").length() > 0) {
 
-                    email_attachments = ((JSONObject) email_json.getJSONArray("attachments").get(0)).getString("filename");
+                    email_attachment1 = ((JSONObject) email_json.getJSONArray("attachments").get(0)).getString("filename");
+                    email_attachment1_id = ((JSONObject) email_json.getJSONArray("attachments").get(0)).getString("id");
+
+                }
+
+                else {
+
+                    email_attachment1 = "";
 
                 }
 
